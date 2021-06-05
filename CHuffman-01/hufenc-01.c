3317ed578f0e55acaefafea18a5b89754727fd17
rald <thecoconutnutisagiantnut@gmail.com>
2021-06-05 09:39:42 +0800

    OK

--- CHuffman-01/hufenc-01.c
+++ CHuffman-01/hufenc-01.c
@@ -2,10 +2,18 @@
 #include <limits.h>
 #include "pqueue.h"
 
-#define DEBUG 0
+#define DEBUG 1
 #define TABLE_MAX 257
 #define BITS_MAX 256
 #define FREQ_MAX 256
+
+
+
+void clearTable(char*** table) {
+	for(int i=0;i<TABLE_MAX;i++) {
+		(*table)[i]=NULL;
+	}
+}
 
 
 
@@ -17,7 +25,7 @@ void BT_Walk(PQ_Node** head, char ***table,int space,char bit) {
         return;
 	}
 
-	for(int i=0;i<space+1;i++) printf("\t");
+if(DEBUG)	for(int i=0;i<space+1;i++) printf("\t");
 
 	if(bit!='X') {
 
@@ -44,8 +52,16 @@ if(DEBUG)		printf("OO->%d->%c\n",(*head)->priority,bit);
 }
 
 
+void BT_WalkX(PQ_Node** head, char ***table) {
+if(DEBUG)	printf("--- BT_WalkX ---\n");
+	clearTable(table);
+	BT_Walk(head,table,-1,'X');
+}
+
+
 
 void BT_PrintTable(char*** table) {
+if(DEBUG)	printf("--- BT_PrintTable ---\n");
 	for(int i=0;i<TABLE_MAX;i++) {
 		if((*table)[i]) {
 	 		if(i==256) {
@@ -59,17 +75,9 @@ if(DEBUG)		printf("%02X->%s\n",i,(*table)[i]);
 
 
 
-void clearTable(char*** table) {
-	for(int i=0;i<TABLE_MAX;i++) {
-		(*table)[i]=NULL;
-	}
-}
-
-
-
 void encode(char ***table,char *filename) {
 
-if(DEBUG)	printf("--- ENCODE ---\n");
+if(DEBUG)	printf("--- encode ---\n");
 
 	FILE *fin=NULL;
 	FILE *fout=NULL;
@@ -118,72 +126,12 @@ if(DEBUG)	printf("%02X ",b);
 if(DEBUG)	printf("%02X ",b);
 	}
 
-	printf("\n");
+if(DEBUG)	printf("\n");
 
 	fclose(fin);
 	fclose(fout);
 
 }
-
-
-
-void decode(char ***table,char *filename) {
-
-if(DEBUG)	printf("--- DECODE ---\n");
-
-	FILE *fin=NULL;
-	FILE *fout=NULL;
-
-	char infile[PATH_MAX]={0};
-	strcpy(infile,filename);
-	strcat(infile,".enc");
-
-	fin=fopen(infile,"rb");
-
-	if(!fin) {
-		printf("cannot open file %s\n",infile);
-		return;
-	}
-
-	char outfile[PATH_MAX]={0};
-	strcpy(outfile,filename);
-	strcat(outfile,".dec");
-
-	fout=fopen(outfile,"wb");
-
-	int ch='\0';
-	int i=0;
-	char b[256]={0};
-	while((ch=fgetc(fin))!=EOF) {
-		for(int j=0;j<8;j++) {
-			b[i]=(((ch&(1<<(7-j)))==0)?'0':'1');
-			b[i+1]='\0';
-			i++;
-			int k=-1;
-			for(int l=0;l<257;l++) {
-				if(((*table)[l]!=NULL) && (!strcmp(b,(*table)[l]))) {
-					k=l;
-					break;
-				}
-			}
-			if(k==256) {
-if(DEBUG)		printf("%d->%s\n",k,b);
-				goto terminate;
-			} else if(k!=-1) {
-				fputc(k,fout);
-if(DEBUG)		printf("%d->%s\n",k,b);
-				i=0;
-				b[0]='\0';
-			}
-		}
-	}
-terminate:
-
-	fclose(fin);
-	fclose(fout);
-}
-
-
 
 void saveTable(PQ_Node** head,FILE *fout) {
 	if((*head)==NULL) return;
@@ -205,6 +153,7 @@ void saveTable(PQ_Node** head,FILE *fout) {
 
 
 void saveTableX(PQ_Node** head,char *filename) {
+if(DEBUG)	printf("--- saveTableX ---\n");
 	char outfile[PATH_MAX];
 	strcpy(outfile,filename);
 	strcat(outfile,".tab");
@@ -214,84 +163,21 @@ void saveTableX(PQ_Node** head,char *filename) {
 }
 
 
+PQ_Node* createHuffmanTree(char *filename) {
 
-void loadTable(char ***table,FILE *fin,int depth,char bit) {
-	static char bits[BITS_MAX]={0};
-	char b1[3]={0};
-	char b2[3]={0};
-	unsigned int v=0;
-
-	if(fscanf(fin,"%2s",b1)==1) {
-
-		if(bit!='X') {
-			bits[depth]=bit;
-			bits[depth+1]='\0';
-		}
-
-		if(!strcmp(b1,"00")) {
-			if(fscanf(fin,"%2s",b2)==1) {
-				if(!strcmp(b2,"XX")) {
-					(*table)[256]=strdup(bits);
-				} else {
-					sscanf(b2,"%x",&v);
-					(*table)[v]=strdup(bits);
-				}
-			} else {
-				return;
-			}
-		} else if(!strcmp(b1,"01")) {
-			loadTable(table,fin,depth+1,'0');
-			loadTable(table,fin,depth+1,'1');
-		}
-	}
-
-}
+if(DEBUG)	printf("--- createHuffmanTree ---\n");
 
 
-
-char** loadTableX(char *filename) {
-	char **table=malloc(sizeof(*table)*TABLE_MAX);
+    PQ_Node *pq=NULL;
 	FILE *fin=NULL;
-
-	char infile[PATH_MAX];
-	strcpy(infile,filename);
-	strcat(infile,".tab");
-
-	fin=fopen(infile,"rt");
-
-	if(!fin) {
-		printf("cannot open file %s.\n",infile);
-		return NULL;
-	}
-
-	clearTable(&table);
-
-	loadTable(&table,fin,-1,'X');
-
-	fclose(fin);
-
-	return table;
-}
-
-
-
-int main(int argc,char **argv) {
-
-	FILE *fin=NULL;
-
 	int ch='\0';
 	int freq[FREQ_MAX]={0};
-    PQ_Node *pq=NULL;
 
-	char **table=malloc(sizeof(*table)*TABLE_MAX);
-
-	clearTable(&table);
-
-	fin=fopen(argv[1],"rb");
+	fin=fopen(filename,"rb");
 
 	if(!fin) {
-		printf("cannot open file %s.\n",argv[1]);
-		return EXIT_FAILURE;
+		printf("cannot open file %s.\n",filename);
+		return NULL;
 	}
 
 	while((ch=fgetc(fin))!=EOF) {
@@ -311,19 +197,23 @@ int main(int argc,char **argv) {
 		PQ_Push(&pq,-1,l->priority+r->priority,l,r);
 	}
 
-	BT_Walk(&pq,&table,-1,'X');
+	return pq;
+}
 
-	BT_PrintTable(&table);
+int main(int argc,char **argv) {
+
+	char **table=malloc(sizeof(*table)*TABLE_MAX);
+	PQ_Node *pq=NULL;
+
+	pq=createHuffmanTree(argv[1]);
+
+	BT_WalkX(&pq,&table);
 
 	saveTableX(&pq,argv[1]);
 
+	BT_PrintTable(&table);
+
 	encode(&table,argv[1]);
-
-	char **table2=loadTableX(argv[1]);
-
-	BT_PrintTable(&table2);
-
-	decode(&table2,argv[1]);
 
     return EXIT_SUCCESS;
 }
